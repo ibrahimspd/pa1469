dce76011ef7c8f52ef59d651de64fb578b42c8b0
Pontusg45 <pontusg204@gmail.com>
2022-11-24 01:37:29 +0100

    -Add start generate lineup activity on button click

--- app/src/main/java/com/example/myapplication/Controller/HomePageActivity.java
+++ app/src/main/java/com/example/myapplication/Controller/HomePageActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,6 +20,14 @@ public class HomePageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
+        GenerateLineupButton = (Button) findViewById(R.id.GenerateLineupButton);
+        GenerateLineupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGenerateLineupActivity();
+            }
+        });
+
         LineupButton = findViewById(R.id.LineupButton);
         LineupButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +38,11 @@ public class HomePageActivity extends AppCompatActivity {
     }
     private void openMyLineupActivity(){
         Intent intent = new Intent(this, MyLineupActivity.class);
+        startActivity(intent);
+    }
+
+    private void openGenerateLineupActivity(){
+        Intent intent = new Intent(this, GenerateLineupActivity.class);
         startActivity(intent);
     }
 }
