fef9cae171ff7f40f7cbc8d3fcd915b36e4bbf6c
Pontusg45 <62245536+Pontusg45@users.noreply.github.com>
2022-12-12 11:59:15 +0100

    Update LoginActivity.java

--- app/src/main/java/com/example/myapplication/Controller/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/Controller/LoginActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import com.example.myapplication.R;
 import com.example.myapplication.database.FirestoreImpl;
 
-public class LoginActivity extends AppCompatActivity 
+public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
