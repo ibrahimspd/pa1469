203851b928dfba8abbc39ffd12b74bdd05b57f59
ibrahimspd <techin.rec@gmail.com>
2022-12-14 18:04:04 +0100

    Add database interface with entites

--- app/src/main/java/com/example/myapplication/database/Database.java
+++ app/src/main/java/com/example/myapplication/database/Database.java
@@ -5,14 +5,16 @@ import com.example.myapplication.entites.Credentials;
 import com.example.myapplication.entites.Lineup;
 import com.example.myapplication.entites.Team;
 
+import java.util.ArrayList;
+
 public interface Database
 {
+
     boolean checkCredentials(Credentials credentials);
     boolean createAccount(Credentials credentials);
     Lineup getLineUp(int teamId);
     Lineup getLineUp(String teamName);
-    Team getTeam(int teamId);
-    Team getTeam(String teamName);
+    void getTeam(OnTeamListener listener);
     
 
 }
--- app/src/main/java/com/example/myapplication/database/FirestoreImpl.java
+++ app/src/main/java/com/example/myapplication/database/FirestoreImpl.java
@@ -1,16 +1,29 @@
 package com.example.myapplication.database;
 
+import static android.content.ContentValues.TAG;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
 import com.example.myapplication.entites.Credentials;
 import com.example.myapplication.entites.Lineup;
 import com.example.myapplication.entites.Team;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-;
+;import java.util.ArrayList;
+import java.util.Map;
 
 public class FirestoreImpl implements Database {
     private static final FirebaseFirestore DB = FirebaseFirestore.getInstance();
     public FirestoreImpl() {
 
     }
+
+
 
     @Override
     public boolean checkCredentials(Credentials credentials) {
@@ -34,12 +47,30 @@ public class FirestoreImpl implements Database {
     }
 
     @Override
-    public Team getTeam(int teamId) {
-        return null;
+    public void getTeam(OnTeamListener listener) {
+        DocumentReference documentReference = DB.collection("teams").document("test");
+
+        final Team[] result = {null};
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    if (documentSnapshot.exists()){
+                        ArrayList<Team> teams = new ArrayList<>();
+                        teams.add(documentSnapshot.toObject(Team.class));
+                        listener.onTeamFilled(teams);
+
+                        return;
+                    }else{
+                        Log.d(TAG, "onComplete: no shit");
+                    }
+                }else{
+                    Log.d(TAG, "failed ", task.getException());
+                }
+            }
+        });
+        Log.d(TAG, "getTeam: ");
     }
 
-    @Override
-    public Team getTeam(String teamName) {
-        return null;
-    }
 }
