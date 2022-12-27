1c44cfd981d7357da2879449c3ca33f09f38ba1b
RobinHellgren01 <robinhellgren0101@hotmail.com>
2022-12-27 11:26:17 +0100

    Add GenerateLineupModelTest file

--- app/src/test/java/com/example/myapplication/Model/GenerateLineupModelTest.java
+++ app/src/test/java/com/example/myapplication/Model/GenerateLineupModelTest.java
@@ -1,4 +1,17 @@
+package com.example.myapplication.Model;
+
 import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
 public class GenerateLineupModelTest {
-  
+    @Test
+    public void addTwoIsCorrect() {
+
+
+    }
+
 }
